7eccaa144aafe72fe19b3ebb8d57bafb8509c686
Thomas-Basadonne <thommybasa@gmail.com>
2023-02-06 16:50:00 +0100

    milestone 1

---
+++ myscript.js
@@ -0,0 +1,49 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const team = [
+  {
+    immagine: "wayne-barnett-founder-ceo.jpg",
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+  },
+  {
+    immagine: "angela-caroll-chief-editor.jpg",
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+  },
+  {
+    immagine: "walter-gordon-office-manager.jpg",
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+  },
+  {
+    immagine: "angela-lopez-social-media-manager.jpg",
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+  },
+  {
+    immagine: "scott-estrada-developer.jpg",
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+  },
+  {
+    immagine: "barbara-ramos-graphic-designer.jpg",
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+  },
+];
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+console.log(team[0], team[1], team[2], team[3], team[4], team[5]);
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
